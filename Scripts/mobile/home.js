2b1ef94bad5205552b74756314a3f765da1e18b0
zhengkanxia <2217382066@qq.com>
2018-09-18 11:19:44 +0800

    new zkx

--- Scripts/mobile/home.js
+++ Scripts/mobile/home.js
@@ -4,7 +4,6 @@ function onHomePage() {
     authorizationName();
     
     switchToolbar("homeTool");
-<<<<<<< HEAD
     //图表
     $("#purchase_Bar").width(window.screen.width);
 
@@ -16,11 +15,6 @@ function onHomePage() {
     VideoBaner("KOvm_container","swiper-paginationTrailer-KOvm",KOvm);//场景
     commonlyUsedFun("pptPattern_container ol","50",pptPattern);//PPT
     commonlyUsedFun("jjPattern_container ol","50",jjPattern);//讲解
-=======
-    
-//  myApp.router.navigate("/equips/")
-//   myApp.router.navigate("/ycAndyx/")
->>>>>>> 8eb2bb0a3458eb001012512caec35f0fc0ccd6e6
 }
 //界面尺寸变化事件
 function onResizeCustomized() {
