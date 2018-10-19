07b17cd4adef2402be28fdeb2765c488829ea7a4
2881704400 <2881704400@qq.com>
2018-10-19 10:15:02 +0800

    合并更新实时快照

--- Scripts/mobile/snapShotDetail.js
+++ Scripts/mobile/snapShotDetail.js
@@ -9,7 +9,7 @@ function snapShotDetail() {
 	var chatTitleName = chatValue[0];
 	chatList = chatValue[1];
 	$("#snapShotDetailTitleId").html(chatTitleName)
-	
+
 	var snapashot_ptr = $$('.snapashotMessage-page-content');
 	snapashot_ptr.on("ptr:refresh", refreshpg);
 	loadMessage();
@@ -17,7 +17,7 @@ function snapShotDetail() {
 	var searchbar = myApp.searchbar.create({
 		el: '.searchbar',
 		searchContainer: '.snapshotMessage-ul',
-		searchIn: '.item-title',
+		searchIn: '.item-title,.item-subtitle',
 	});
 }
 
@@ -63,68 +63,73 @@ function loadMessage() {
 				var strSureData = "";
 				var strData = "";
 				var countNum = 0;
-				for(var i = 0; i < result.length; i++) {
-					var textareaEventMsg = "";
-					if(result[i].EventMsg.length > 200) {
-						textareaEventMsg = "<textarea>" + result[i].EventMsg + "</textarea>";
-					} else {
-						textareaEventMsg = result[i].EventMsg;
-					}
+				if(result.length > 0) {
+					for(var i = 0; i < result.length; i++) {
+						var textareaEventMsg = "";
+						if(result[i].EventMsg.length > 200) {
+							textareaEventMsg = "<textarea>" + result[i].EventMsg + "</textarea>";
+						} else {
+							textareaEventMsg = result[i].EventMsg;
+						}
 
-					var textareaAdviceMsg = "";
-					if(result[i].Proc_advice_Msg.length > 200) {
-						textareaAdviceMsg = "<textarea>" + result[i].Proc_advice_Msg + "</textarea>";
-					} else {
-						textareaAdviceMsg = result[i].Proc_advice_Msg;
-					}
+						var textareaAdviceMsg = "";
+						if(result[i].Proc_advice_Msg.length > 200) {
+							textareaAdviceMsg = "<textarea>" + result[i].Proc_advice_Msg + "</textarea>";
+						} else {
+							textareaAdviceMsg = result[i].Proc_advice_Msg;
+						}
 
-					var isSureSpan = "";
-					if(result[i].bConfirmed == false) {
-						isSureSpan = "<span class='span-color-notsure sure-flag'>未确认</span>";
-						strData += '<li class="accordion-item">' +
-							'<a href="#" class="item-link item-content">' +
-							'	<div class="item-inner">' +
-							'		<div class="item-title-row">' +
-							'			<div class="item-subtitle">' + formatDate(result[i].Time) + '</div>' +
-							'			<div class="item-after">' + isSureSpan + '</div>' +
-							'		</div>' +
-							'		<div class="item-text item-title fontweight-normal">' + result[i].EventMsg + '</div>' +
-							'	</div>' +
-							'</a>' +
-							'<div class="accordion-item-content content-container">' +
-							'<div class="content-container-block">' +
-							'<p>时间：' + formatDate(result[i].Time) + '</p>' +
-							'<p>事件：' + textareaEventMsg + '</p>' +
-							'<p>处理意见：<textarea class="advice-textarea" placeholder="请输入处理意见"></textarea></p>' +
-							'<p>是否发送短信：&nbsp;&nbsp;<label class="toggle toggle-init color-blue" onclick="onProcsCheckBox(' + countNum + ')">' +
-							'<input type="checkbox" class="isProcsInput"><span class="toggle-icon"></span></label><div class="procsContent list-block" style="height:auto;display:block"></div></p>' +
-							"<p><a href='#' class=\"button button-big button-fill color-blue\" onclick='OnSureMessage(\"" + countNum + "\",\"" + textareaEventMsg + "\",\"" + result[i].Time + "\")' values='" + result[i] + "' title=\"" + result[i].User_Confirmed + formatDate(result[i].Dt_Confirmed) + "\">确定</a></p>" +
-							'</div></div>' +
-							'</li>';
-						countNum++;
-					} else {
-						isSureSpan = "<span class='span-color-sure sure-flag'>已确认</span>";
-						strSureData += '<li class="accordion-item">' +
-							'<a href="#" class="item-link item-content">' +
-							'	<div class="item-inner">' +
-							'		<div class="item-title-row">' +
-							'			<div class="item-subtitle">' + formatDate(result[i].Time) + '</div>' +
-							'			<div class="item-after">' + isSureSpan + '</div>' +
-							'		</div>' +
-							'		<div class="item-text item-title fontweight-normal">' + result[i].EventMsg + '</div>' +
-							'	</div>' +
-							'</a>' +
-							'<div class="accordion-item-content content-container">' +
-							'<div class="content-container-block">' +
-							'<p>时间：' + formatDate(result[i].Time) + '</p>' +
-							'<p>事件：' + textareaEventMsg + '</p>' +
-							'<p>处理意见：' + textareaAdviceMsg + '</p>' +
-							"<p>确认人：" + result[i].User_Confirmed + '</p><p>确认时间：' + formatDate(result[i].Dt_Confirmed) + "</p>" +
-							'</div></div>' +
-							'</li>';
+						var isSureSpan = "";
+						if(result[i].bConfirmed == false) {
+							isSureSpan = "<span class='span-color-notsure sure-flag'>未确认</span>";
+							strData += '<li class="accordion-item">' +
+								'<a href="#" class="item-link item-content">' +
+								'	<div class="item-inner">' +
+								'		<div class="item-title-row">' +
+								'			<div class="item-subtitle">' + formatDate(result[i].Time) + '</div>' +
+								'			<div class="item-after">' + isSureSpan + '</div>' +
+								'		</div>' +
+								'		<div class="item-text item-title fontweight-normal">' + result[i].EventMsg + '</div>' +
+								'	</div>' +
+								'</a>' +
+								'<div class="accordion-item-content content-container">' +
+								'<div class="content-container-block">' +
+								'<p>时间：' + formatDate(result[i].Time) + '</p>' +
+								'<p>事件：' + textareaEventMsg + '</p>' +
+								'<p>处理意见：<textarea class="advice-textarea" placeholder="请输入处理意见"></textarea></p>' +
+								'<p>是否发送短信：&nbsp;&nbsp;<label class="toggle toggle-init color-blue" onclick="onProcsCheckBox(' + countNum + ')">' +
+								'<input type="checkbox" class="isProcsInput"><span class="toggle-icon"></span></label><div class="procsContent list-block" style="height:auto;display:block"></div></p>' +
+								"<p><a href='#' class=\"button button-big button-fill color-blue\" onclick='OnSureMessage(\"" + countNum + "\",\"" + textareaEventMsg + "\",\"" + result[i].Time + "\")' values='" + result[i] + "' title=\"" + result[i].User_Confirmed + formatDate(result[i].Dt_Confirmed) + "\">确定</a></p>" +
+								'</div></div>' +
+								'</li>';
+							countNum++;
+						} else {
+							isSureSpan = "<span class='span-color-sure sure-flag'>已确认</span>";
+							strSureData += '<li class="accordion-item">' +
+								'<a href="#" class="item-link item-content">' +
+								'	<div class="item-inner">' +
+								'		<div class="item-title-row">' +
+								'			<div class="item-subtitle">' + formatDate(result[i].Time) + '</div>' +
+								'			<div class="item-after">' + isSureSpan + '</div>' +
+								'		</div>' +
+								'		<div class="item-text item-title fontweight-normal">' + result[i].EventMsg + '</div>' +
+								'	</div>' +
+								'</a>' +
+								'<div class="accordion-item-content content-container">' +
+								'<div class="content-container-block">' +
+								'<p>时间：' + formatDate(result[i].Time) + '</p>' +
+								'<p>事件：' + textareaEventMsg + '</p>' +
+								'<p>处理意见：' + textareaAdviceMsg + '</p>' +
+								"<p>确认人：" + result[i].User_Confirmed + '</p><p>确认时间：' + formatDate(result[i].Dt_Confirmed) + "</p>" +
+								'</div></div>' +
+								'</li>';
+						}
 					}
+					$("#snapShotDetailListId").html(strData + strSureData);
+					$("#snapShotDetailListId").css({
+						"margin-bottom": "66px"
+					})
 				}
-				$("#snapShotDetailListId").html(strData + strSureData);
 			}
 		}
 	});
