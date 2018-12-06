bd422ee3145a416d1935e07843fbec6712ea6dff
2881704400 <2881704400@qq.com>
2018-12-06 13:29:15 +0800

    语音初版v1.0

--- Scripts/mobile/pageJavascript/Voice.js
+++ Scripts/mobile/pageJavascript/Voice.js
@@ -1,177 +1,321 @@
-﻿//点击事件
-$("#voiceContainer,.voicePanel,.voicePanel>a").unbind();
-$("#voiceContainer,.voicePanel>a").bind("click",function(){
-   $("#voiceContainer").addClass("voiceContainer");
-   return false;
+﻿var isVoices = false;
+var cancelFlag = false;
+var startX, //触摸时的坐标   
+	startY,
+	x, //滑动的距离   
+	y,
+	aboveY = 0; //设一个全局变量记录上一次内部块滑动的位置    
+$(function() {
+
+	$$('.popup-voices').on('popup:open', function(e, popup) {
+		$(".view-main").css({
+			filter: 'blur(8px)'
+		})
+		$(".voice-container").html('<div class="pannel-chat-info">' +
+			'	<div class="chart-content chart-content-old">' +
+			'		<span>请告诉我，您想要进行的操作</span>' +
+			'	</div>' +
+			'</div>');
+	});
+
+	$$('.popup-voices').on('popup:close', function(e, popup) {
+		$(".view-main").css({
+			filter: 'blur(0px)'
+		})
+	});
+
+	document.getElementById("videoContentBtnId").addEventListener('touchstart', onTouchStart);
+	document.getElementById("videoContentBtnId").addEventListener('touchend', onTouchEnd);
+	document.getElementById("videoContentBtnId").addEventListener('touchmove', onTouchMove);
+	//记录选择
+	if(!window.localStorage.voiceList) {
+		window.localStorage.voiceList = "1";
+	}
+
+	try {
+		myJavaFun.VoiceOpen();
+	} catch(ex) {
+
+	}
 });
 
-$(".voicePanel").bind("click",function(){
-   return false;
-});
-//监听
-document.getElementById("voiceBtn").addEventListener('touchstart', onTouchStart);
-document.getElementById("voiceBtn").addEventListener('touchend', onTouchEnd);
-try {
-    myJavaFun.VoiceOpen();
-}
-catch (ex) {
-
-}
-var isVoices = false;
-//按住开始说话
-function onTouchStart() {
-
-    //myApp.toast.close();
-    $("#voiceContainer").removeClass("voiceContainer");
-    $(this).addClass("voiceActive");
-    $("#voiceMessage").hide();
-    $("#waveAnim").show();
-    try {
-        isVoices = true;
-        if (window.localStorage.voiceList == "0") {
-            myJavaFun.StartVice();
-        }
-        else {
-            myJavaFun.StartVoice(window.localStorage.voiceList);
-        }
-    } catch (ex) {
-        // $("#voiceMessage").html("无法使用此功能，请下载最新app！");
-        //voiceTooip("无法使用此功能，请下载最新app！").open();
-    }
+function onTouchStart(e) {
+	e.preventDefault();
+	var touch = e.touches[0];
+	startY = touch.pageY; //刚触摸时的坐标
+	$(".voice-container").html('<div class="pannel-chat-info">' +
+		'	<div class="chart-content chart-content-old">' +
+		'		<span>请告诉我，您想要进行的操作</span>' +
+		'	</div>' +
+		'</div>');
+	$(".voice-arrow-cancel").show();
+	$(".voice-arrow-box").show();
+	$(".voice-arrow-dialog").hide();
+	try {
+		isVoices = true;
+		if(window.localStorage.voiceList == "0") {
+			myJavaFun.StartVice();
+		} else {
+			myJavaFun.StartVoice(window.localStorage.voiceList);
+		}
+	} catch(ex) {}
+	$(".voice-container").append('<div class="pannel-chat-info">' +
+		'	<div class="chart-content stay-right">' +
+		'		<div class="waveAnim"><i></i><i></i><i></i><i></i><i></i></div>' +
+		'	</div>' +
+		'</div>');
+	$('.voice-container').scrollTop($('.voice-container')[0].scrollHeight);
 }
 
-//释放手指并识别语音
-function onTouchEnd() {
-    
-    if (!isVoices) {
-        return;
-    }
-    if ($(this).hasClass("voiceActive")) {
-        $(this).removeClass("voiceActive");
-        $("#voiceMessage").show();
-        $("#voiceMessage").html("正在识别…");
-        // voiceTooip("正在识别…").open();
-        $("#waveAnim").hide();
-    }
+function onTouchMove(e) {
+	e.preventDefault();
+	var touch = e.touches[0];
+	y = touch.pageY - startY; //滑动的距离
+	var distance = aboveY + y;
+	console.log(distance, y)
+	if(distance < -10 && distance > -200) {
+		/*$(".voice-arrow-box").css({
+			bottom: 70 + Math.abs(distance) + "px",
+			opacity: 1 - Math.abs(distance) * 0.01
+		});*/
 
-    document.getElementById("voiceBtn").removeEventListener('touchstart', onTouchStart);
-    document.getElementById("voiceBtn").removeEventListener('touchend', onTouchEnd);
-    setTimeout(function () {
-        try {
-            if (window.localStorage.voiceList == "0") {
-                myJavaFun.StopVice();
-            }
-            else {
-                myJavaFun.StopVoice();
-            }
-        } catch (ex) {
-            isVoices = false;
-            $("#voiceMessage").html("无法使用此功能，请下载最新app！");
-            // voiceTooip("无法使用此功能，请下载最新app！").open();
-            document.getElementById("voiceBtn").addEventListener('touchstart', onTouchStart);
-            document.getElementById("voiceBtn").addEventListener('touchend', onTouchEnd);
-            setTimeout(function () {
-                if (isVoices == false) {
-                    $("#voiceMessage").html("按住说话");
-                    //voiceTooip("按住说话").open();
-                    document.getElementById("voiceBtn").addEventListener('touchstart', onTouchStart);
-                    document.getElementById("voiceBtn").addEventListener('touchend', onTouchEnd);
-                }
-            }, 3000);
-        }
-    }, 50);
+		if(distance < -70 && !cancelFlag) {
+			$(".voice-arrow-cancel").css({
+				background: "rgba(255,255,255,1)",
+				color: "rgba(112,112,112,1)"
+			});
+			$(".voice-arrow-box .voice-arrow-ani").each(function() {
+				$(this).removeClass().addClass("voice-arrow-ani");
+				$(this).css({
+					opacity: 1
+				});
+			});
+			cancelFlag = true;
+		}
+	}
+}
+
+function onTouchEnd(e) {
+	if(cancelFlag) {
+		$(".voice-arrow-cancel").hide();
+		$(".voice-arrow-box").hide();
+		$(".voice-arrow-dialog").show();
+		$(".voice-arrow-box").css({
+			bottom: "70px",
+			opacity: 1
+		});
+		$(".voice-arrow-cancel").css({
+			background: "rgba(255,255,255,0)",
+			color: "rgba(255,255,255,1)"
+		});
+		$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>指令已取消</span>");
+
+		$(".voice-arrow-box .voice-arrow-ani").each(function() {
+			var index=$(this).index();
+			index=3-index;
+			$(this).removeClass().addClass("voice-arrow-ani IndexIconAnimation"+index+"");
+		});
+		try {
+			if(window.localStorage.voiceList == "0") {
+				myJavaFun.StopVice();
+			} else {
+				myJavaFun.StopVoice();
+			}
+		} catch(ex) {
+			isVoices = false;
+			$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>无法使用此功能，请下载最新app！</span>");
+			document.getElementById("voiceBtn").addEventListener('touchstart', onTouchStart);
+			document.getElementById("voiceBtn").addEventListener('touchend', onTouchEnd);
+			setTimeout(function() {
+				if(isVoices == false) {
+					document.getElementById("voiceBtn").addEventListener('touchstart', onTouchStart);
+					document.getElementById("voiceBtn").addEventListener('touchend', onTouchEnd);
+				}
+			}, 3000);
+		}
+		return;
+	} else {
+		$(".voice-arrow-cancel").hide();
+		$(".voice-arrow-box").hide();
+		$(".voice-arrow-dialog").show();
+		$(".voice-arrow-box").css({
+			bottom: "70px",
+			opacity: 1
+		});
+		$(".voice-arrow-cancel").css({
+			background: "rgba(255,255,255,0)",
+			color: "rgba(255,255,255,1)"
+		});
+	}
+	if(!isVoices) {
+		return;
+	}
+	$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>正在识别..</span>");
+
+	document.getElementById("videoContentBtnId").removeEventListener('touchstart', onTouchStart);
+	document.getElementById("videoContentBtnId").removeEventListener('touchend', onTouchEnd);
+	setTimeout(function() {
+		try {
+			if(window.localStorage.voiceList == "0") {
+				myJavaFun.StopVice();
+			} else {
+				myJavaFun.StopVoice();
+			}
+		} catch(ex) {
+			isVoices = false;
+			$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>无法使用此功能，请下载最新app！</span>");
+			$('.voice-container').scrollTop($('.voice-container')[0].scrollHeight);
+			document.getElementById("videoContentBtnId").addEventListener('touchstart', onTouchStart);
+			document.getElementById("videoContentBtnId").addEventListener('touchend', onTouchEnd);
+			setTimeout(function() {
+				if(isVoices == false) {
+					document.getElementById("videoContentBtnId").addEventListener('touchstart', onTouchStart);
+					document.getElementById("videoContentBtnId").addEventListener('touchend', onTouchEnd);
+				}
+			}, 3000);
+		}
+	}, 50);
+
+}
+
+function StartVoiceXF() {
+	try {
+		myJavaFun.StartViceXF(parseInt(window.localStorage.XFOffline));
+	} catch(ex) {
+		//		$("#voiceMessage_xf1").html("无法使用此功能！");
+	}
+}
+
+function callbackVoiceXFMessage(dt) {
+	if(cancelFlag) {
+		cancelFlag = false;
+		return;
+	}
+	$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>" + dt + "</span>");
+	$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').show();
+	$("#waveAnim").hide();
+	isVoices = false;
+	document.getElementById("videoContentBtnId").addEventListener('touchstart', onTouchStart);
+	document.getElementById("videoContentBtnId").addEventListener('touchend', onTouchEnd);
+}
+
+function callbackVoiceXFData(dt) {
+	//	 myApp.dialog.alert(dt);
+	var _url = service + "/VoiceControlString";
+	var _data = "audioData=" + dt + "&&userName=" + window.localStorage.userName;
+	ajaxService("post", _url, true, _data, _successf, _error);
+
+	function _successf(data) {
+		var rets = $(data).children("string").text();
+		if(rets == "") {
+			$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>未识别！</span>");
+		} else {
+
+			rets = rets.replace("未识别语音,内容---", "");
+			rets = rets.replace("。", "");
+			$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>" + rets + "</span>");
+			setTimeout(function() {
+				$(".voice-container").append('<div class="pannel-chat-info">' +
+					'	<div class="chart-content">' +
+					'		<span>好的，开始执行：' + rets + '..</span>' +
+					'	</div>' +
+					'</div>');
+				$(".voice-container").append('<div class="pannel-chat-info">' +
+					'	<div class="chart-content">' +
+					'		<span>' + rets + '已执行</span>' +
+					'	</div>' +
+					'</div>');
+				$('.voice-container').scrollTop($('.voice-container')[0].scrollHeight);
+			}, 500);
+		}
+		isVoices = false;
+		document.getElementById("videoContentBtnId").addEventListener('touchstart', onTouchStart);
+		document.getElementById("videoContentBtnId").addEventListener('touchend', onTouchEnd);
+	}
+
+	function _error(qXHR, textStatus, errorThrown) {
+		$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>服务器出错！</span>");
+		isVoices = false;
+		document.getElementById("videoContentBtnId").addEventListener('touchstart', onTouchStart);
+		document.getElementById("videoContentBtnId").addEventListener('touchend', onTouchEnd);
+		setTimeout(function() {
+			if(isVoices == false) {
+				$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>按住说话</span>");
+			}
+		}, 3000);
+	}
+}
+
+function ajaxServiceSend(_type, _url, _asycn, _data, _beforeSend, _success, _error) {
+	var ajaxs = $.ajax({
+		type: _type,
+		url: _url,
+		timeout: 5000,
+		async: _asycn,
+		data: _data,
+		beforeSend: _beforeSend,
+		success: _success,
+		error: _error,
+		complete: function(XMLHttpRequest, status) { //请求完成后最终执行参数
+			if(status == 'timeout') { //超时,status还有success,error等值的情况
+				ajaxs.abort();
+				console.log("超时");
+				// myApp.hideIndicator();
+				myApp.dialog.create({
+					title: "系统提示",
+					text: '请求超时，请查看网络是否已连接！',
+					buttons: [{
+						text: '确定'
+					}]
+				}).open();
+			}
+			XMLHttpRequest = null;
+		}
+	});
 }
 
 //接收回调数据并上传至服务器
 function callbackVoiceBuffer(dt) {
-    var _url = service + "/VoiceControlByte";
-    var _data = "audioData=" + dt + "&&userName=" + window.localStorage.userName;
-    ajaxService("post", _url, true, _data, _successf, _error);
-    function _successf(data) {
-        var rets = $(data).children("string").text();
-        if (rets == "") {
-            $("#voiceMessage").html("未识别！");
-            // voiceTooip("未识别！").open();
-        }
-        else {
-            $("#voiceMessage").html(rets);
-            // voiceTooip(rets).open();
-        }
-        isVoices = false;
-        document.getElementById("voiceBtn").addEventListener('touchstart', onTouchStart);
-        document.getElementById("voiceBtn").addEventListener('touchend', onTouchEnd);
-    }
-    function _error(qXHR, textStatus, errorThrown) {
-        $("#voiceMessage").html("服务器出错！");
-        // voiceTooip("服务器出错！").open();
-        isVoices = false;
-        document.getElementById("voiceBtn").addEventListener('touchstart', onTouchStart);
-        document.getElementById("voiceBtn").addEventListener('touchend', onTouchEnd);
-        setTimeout(function () {
-            if (isVoices == false) {
-                $("#voiceMessage").html("按住说话");
-                // voiceTooip("按住说话").open();
-            }
-        }, 3000);
-    }
-}
+	var _url = service + "/VoiceControlByte";
+	var _data = "audioData=" + dt + "&&userName=" + window.localStorage.userName;
+	ajaxService("post", _url, true, _data, _successf, _error);
 
-function StartVoiceXF() {
-    try {
-        $("#voiceBtn_xf").unbind();
-        $("#voiceBtn_xf").addClass("disabled");
-        $("#waveAnim_xf").show();
-        $("#voiceMessage_xf1").hide();
-        $("#voiceMessage_xf2").hide();
-        myJavaFun.StartViceXF(parseInt(window.localStorage.XFOffline));
-    }
-    catch (ex) {
-        $("#waveAnim_xf").hide();
-        $("#voiceMessage_xf1").html("无法使用此功能！");
-        $("#voiceBtn_xf").removeClass("disabled");
-        $("#voiceMessage_xf1").show();
-        $("#voiceMessage_xf2").html("");
-        $("#voiceMessage_xf2").show();
-    }
-}
-function callbackVoiceXFMessage(dt) {
-    $("#voiceMessage").html(dt);
-    // voiceTooip(dt).open();
-    $("#voiceMessage").show();
-    $("#waveAnim").hide();
-    isVoices = false;
-    document.getElementById("voiceBtn").addEventListener('touchstart', onTouchStart);
-    document.getElementById("voiceBtn").addEventListener('touchend', onTouchEnd);
-}
-function callbackVoiceXFData(dt) {
-    var _url = service + "/VoiceControlString";
-    var _data = "audioData=" + dt + "&&userName=" + window.localStorage.userName;
-    ajaxService("post", _url, true, _data, _successf, _error);
-    function _successf(data) {
-        var rets = $(data).children("string").text();
-        if (rets == "") {
-            $("#voiceMessage").html("处理：未识别！");
-            // voiceTooip("处理：未识别！").open();
-        }
-        else {
-            $("#voiceMessage").html("处理：" + rets);
-            // voiceTooip("处理：" + rets).open();
-        }
-        isVoices = false;
-        document.getElementById("voiceBtn").addEventListener('touchstart', onTouchStart);
-        document.getElementById("voiceBtn").addEventListener('touchend', onTouchEnd);
-    }
-    function _error(qXHR, textStatus, errorThrown) {
-        $("#voiceMessage").html("服务器出错！");
-        // voiceTooip("服务器出错！").open();
-        isVoices = false;
-        document.getElementById("voiceBtn").addEventListener('touchstart', onTouchStart);
-        document.getElementById("voiceBtn").addEventListener('touchend', onTouchEnd);
-        setTimeout(function () {
-            if (isVoices == false) {
-                $("#voiceMessage").html("按住说话");
-                // voiceTooip("按住说话").open();
-            }
-        }, 3000);
-    }
+	function _successf(data) {
+		var rets = $(data).children("string").text();
+		if(rets == "") {
+			$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>未识别！</span>");
+		} else {
+			rets = rets.replace("未识别语音,内容---", "");
+			rets = rets.replace("。", "");
+			$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>" + rets + "</span>");
+			setTimeout(function() {
+				$(".voice-container").append('<div class="pannel-chat-info">' +
+					'	<div class="chart-content">' +
+					'		<span>好的，开始执行：' + rets + '..</span>' +
+					'	</div>' +
+					'</div>');
+				$(".voice-container").append('<div class="pannel-chat-info">' +
+					'	<div class="chart-content">' +
+					'		<span>' + rets + '已执行</span>' +
+					'	</div>' +
+					'</div>');
+				$('.voice-container').scrollTop($('.voice-container')[0].scrollHeight);
+			}, 500);
+		}
+		isVoices = false;
+		document.getElementById("videoContentBtnId").addEventListener('touchstart', onTouchStart);
+		document.getElementById("videoContentBtnId").addEventListener('touchend', onTouchEnd);
+	}
+
+	function _error(qXHR, textStatus, errorThrown) {
+		$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>服务器出错！</span>");
+		isVoices = false;
+		document.getElementById("videoContentBtnId").addEventListener('touchstart', onTouchStart);
+		document.getElementById("videoContentBtnId").addEventListener('touchend', onTouchEnd);
+		setTimeout(function() {
+			if(isVoices == false) {
+				$(".voice-container").children(".pannel-chat-info:last-child").find('.chart-content').html("<span>按住说话</span>");
+			}
+		}, 3000);
+	}
 }
